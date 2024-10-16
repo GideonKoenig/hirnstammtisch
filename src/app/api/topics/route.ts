dbb97bed7b8bfb3758708ec727b8bedaf399be6d
Gideon Koenig <gideon.koenig@online.de>
2024-10-16 18:40:44 +0200

    fix: route is getting cached

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const revalidate = 0;
+
 export async function GET() {
     const topics = (await db.query.topics.findMany()).sort((a, b) => b.id - a.id);
     return Response.json({ topics });
