fb506985ee91e439ea7a4b5b6eb4139858c3dc8a
Gideon Koenig <gideon.koenig@online.de>
2024-10-18 22:35:27 +0200

    fix: eslint ignore next config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
-/* eslint-disable */
-
 import { withPlausibleProxy } from "next-plausible";
-
 await import("./src/env.js");
-
-/**
- * @typedef {import('webpack').RuleSetRule} RuleSetRule
- * @typedef {import('webpack').Configuration} WebpackConfig
- */
-
-/**
- * Type guard to check if a rule is a RuleSetRule that tests for ".svg"
- * @param {false | "" | 0 | RuleSetRule | "..." | null | undefined} rule
- * @returns {rule is RuleSetRule}
- */
-function isSvgRule(rule) {
-    return (
-        typeof rule === "object" &&
-        rule !== null &&
-        rule.test instanceof RegExp &&
-        rule.test.test(".svg")
-    );
-}
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
@@ -38,60 +16,49 @@ const config = {
             },
         },
     },
-    webpack(
-        /**
-         * @type {import('webpack').Configuration}
-         */ config,
-    ) {
-        const fileLoaderRule = config.module?.rules?.find(
-            (rule) =>
-                typeof rule === "object" &&
-                rule !== null &&
-                rule.test instanceof RegExp &&
-                rule.test.test(".svg"),
-        );
+    /**
+     * @param {{ module: { rules: any[]; }; }} config
+     */
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
 
-        if (isSvgRule(fileLoaderRule)) {
-            config.module?.rules?.push(
-                {
-                    ...fileLoaderRule,
-                    test: /\.svg$/i,
-                    resourceQuery: /url/,
-                },
-                {
-                    test: /\.svg$/i,
-                    issuer: fileLoaderRule.issuer,
-                    resourceQuery: {
-                        // @ts-ignore
-                        not: [...fileLoaderRule.resourceQuery.not, /url/],
-                    },
-                    use: [
-                        {
-                            loader: "@svgr/webpack",
-                            options: {
-                                svgo: true,
-                                svgoConfig: {
-                                    plugins: [
-                                        {
-                                            name: "preset-default",
-                                            params: {
-                                                overrides: {
-                                                    removeViewBox: false,
-                                                },
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: [
+                    {
+                        loader: "@svgr/webpack",
+                        options: {
+                            svgo: true,
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        name: "preset-default",
+                                        params: {
+                                            overrides: {
+                                                removeViewBox: false,
                                             },
                                         },
-                                    ],
-                                },
-                                typescript: true,
-                                jsxRuntime: "classic",
+                                    },
+                                ],
                             },
+                            typescript: true,
+                            jsxRuntime: "classic",
                         },
-                    ],
-                },
-            );
+                    },
+                ],
+            },
+        );
 
-            fileLoaderRule.exclude = /\.svg$/i;
-        }
+        fileLoaderRule.exclude = /\.svg$/i;
+
         return config;
     },
 };
