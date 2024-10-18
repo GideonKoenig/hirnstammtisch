eebd43002675ac1f368ffc5c973bf50a3d6464d7
Gideon Koenig <gideon.koenig@online.de>
2024-10-18 15:28:43 +0200

    fix: topic endpoint wrong table name

--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -3,6 +3,6 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-    const topics = (await db.query.topics.findMany()).sort((a, b) => b.id - a.id);
+    const topics = (await db.query.TopicsTable.findMany()).sort((a, b) => b.id - a.id);
     return Response.json({ topics });
 }
