20f824658af9e961dcda8b294de19ad33328bbba
Gideon Koenig <gideon.koenig@online.de>
2024-10-29 22:54:02 +0100

    fix: update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tabWidth: 4,
-  printWidth: 100,
+    plugins: ["prettier-plugin-tailwindcss"],
+    tabWidth: 4,
+    printWidth: 80,
 };
 
 export default config;
