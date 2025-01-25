4c5cf382b622952a305973d133ec843c2cc3d76a
Gideon Koenig <gideon.koenig@online.de>
2025-01-25 02:31:00 +0100

    fix: await in sync function

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 
-export function readCookie(name: string) {
+export async function readCookie(name: string) {
     const cookieStore = await cookies();
     return cookieStore.get(name)?.value ?? undefined;
 }
