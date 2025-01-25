37696daa465c2e288b099896665652663c587854
Gideon Koenig <gideon.koenig@online.de>
2025-01-25 02:29:37 +0100

    fix: cookie access is still sync

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 
 export function readCookie(name: string) {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     return cookieStore.get(name)?.value ?? undefined;
 }
