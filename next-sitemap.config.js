3cb68ea8e062c86f6b751453dd01b8a9367a712b
Gideon Koenig <gideon.koenig@online.de>
2025-01-26 14:28:01 +0100

    fix: sitemap is not being generated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,26 @@
 /** @type {import('next-sitemap').IConfig} */
-const sitemapConfig = {
+const config = {
     siteUrl: "https://hirnstammtisch.com",
     generateRobotsTxt: true,
     generateIndexSitemap: false,
+    changefreq: "daily",
+    priority: 0.7,
+    exclude: ["/api/*", "/profile/*"],
+    additionalPaths: async () => [
+        { loc: "/", priority: 1.0 },
+        { loc: "/about", priority: 0.8 },
+        { loc: "/calendar", priority: 0.8 },
+        { loc: "/events", priority: 0.9 },
+    ],
     robotsTxtOptions: {
         policies: [
             {
                 userAgent: "*",
-                disallow: [],
+                disallow: ["/api/*", "/profile/*"],
+                allow: "/",
             },
         ],
     },
 };
 
-export default sitemapConfig;
+export default config;
