447a53f280eafd2c323249fcf4175c3810f522cd
Gideon Koenig <gideon.koenig@online.de>
2025-01-26 13:15:47 +0100

    fix: sign in page layout is broken

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,31 +19,31 @@ export default function Login() {
             return;
         }
 
-        document.cookie = `username=${username.trim()}`;
         await addUser(username.trim());
+        document.cookie = `username=${username.trim()}`;
         router.push("/");
     };
 
     return (
-        <div className="w-dvh flex h-dvh flex-col items-center justify-center p-2">
-            <div className="flex flex-col gap-4 rounded-lg border border-menu-hover bg-menu-light p-4 shadow-sm shadow-menu-dark">
+        <div className="flex h-full w-full flex-col items-center justify-center p-2">
+            <div className="border-menu-hover bg-menu-light shadow-menu-dark flex flex-col gap-4 rounded-lg border p-4 shadow-sm">
                 <p className="w-full text-center text-2xl font-bold">Login</p>
                 <p className="pr-28">To continue please tell us your name.</p>
                 <form className="flex flex-col gap-4" onSubmit={saveName}>
                     <input
                         name="name"
-                        className="w-full rounded-md border border-menu-light bg-menu-dark p-2 px-4 shadow-sm shadow-menu-dark placeholder:text-text-muted focus-visible:outline-hidden"
+                        className="border-menu-light bg-menu-dark shadow-menu-dark placeholder:text-text-muted w-full rounded-md border p-2 px-4 shadow-sm focus-visible:outline-hidden"
                         type="text"
                         placeholder="Your Name"
                     />
                     <button
                         disabled={loading}
-                        className="hover:bg-accent-dark disabled:bg-accent-dark flex w-full flex-row items-center justify-center gap-1 rounded-lg bg-blue-500 p-1 shadow-sm shadow-menu-dark hover:bg-blue-500/80 hover:text-text-muted disabled:bg-blue-500/80 disabled:text-text-muted"
+                        className="hover:bg-accent-dark disabled:bg-accent-dark shadow-menu-dark hover:text-text-muted disabled:text-text-muted flex w-full flex-row items-center justify-center gap-1 rounded-lg bg-blue-500 p-1 shadow-sm hover:bg-blue-500/80 disabled:bg-blue-500/80"
                         type="submit"
                     >
                         <LoaderCircle
                             data-state={loading ? "show" : "hide"}
-                            className="h-4 w-4 animate-spin stroke-text-muted stroke-2 data-[state=hide]:hidden"
+                            className="stroke-text-muted h-4 w-4 animate-spin stroke-2 data-[state=hide]:hidden"
                         />
                         Submit
                     </button>
