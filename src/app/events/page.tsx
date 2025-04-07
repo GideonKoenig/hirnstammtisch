a3257e936d72567b4d3bb7ac3fdb808d5a1f40d3
Gideon Koenig <gideon.koenig@online.de>
2025-04-07 16:24:50 +0200

    feat: move auth check to page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
 import { EventList } from "~/components/event-list";
+import { readCookie } from "~/server/utils";
 
 export default async function EventPage() {
+    const userName = await readCookie("username");
+    if (!userName) {
+        redirect("/login");
+    }
+
     return (
         <div className="flex h-full w-full flex-col">
             <EventList />
